d28c84b317c28eb9343b14c97d98fd7dbcdea66c
Sabbir <webexpertsabbir@gmail.com>
2022-08-16 10:46:59 +0600

    user

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
+// step 1
 document.getElementById('btn-submit').addEventListener('click', function(){
-    console.log('Enter Your email');
+    //step 2
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // console.log(email);
+    // step 3
+    const passwordField = document.getElementById('user-pass');
+    const password = passwordField.value;
+    // console.log(password);
+    // step 4
+    if(email === 'wpdevsabbir@gmail.com' && password === 'wpdevsabbir'){
+        console.log('valid user');
+    }
+    else(
+        console.log('Invalid User')
+    )
 })
