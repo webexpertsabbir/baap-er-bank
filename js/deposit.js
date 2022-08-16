7fa971861fd4e588c2bd9e9630becd1ed4ac8189
Sabbir <webexpertsabbir@gmail.com>
2022-08-16 21:07:53 +0600

    deposit

---
+++ js/deposit.js
@@ -0,0 +1,29 @@
+// step: 1 add even Listener to the buttin
+
+document.getElementById('diposit-button').addEventListener('click', function(){
+    // console.log('Deposit now')
+    // step 2 get deposit input field
+    const depositField = document.getElementById('deposit-field');
+    const newDepositField = depositField.value;
+    const newDepositAmound = parseFloat(newDepositField);
+    // console.log( newDepositAmound)
+    // step 3 get deposit total
+    const depositTotal = document.getElementById('deposit-total');
+    const previesDepositTotalString = depositTotal.innerText;
+    const previesDepositTotal = parseFloat(previesDepositTotalString);
+    // step 5 add total deposit
+    const carentDepositTotal = previesDepositTotal + newDepositAmound;
+    depositTotal.innerText = carentDepositTotal;
+
+    // step 5 get total balance
+    
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceStirng = totalBalance.innerText;
+    const previesTotalBlance = parseFloat(totalBalanceStirng);
+    // step 6 carent total balance
+    const carentTotalBlance = previesTotalBlance + newDepositAmound;
+   // step 7 set total balace
+    totalBalance.innerText = carentTotalBlance;
+    // step 8 cleat input field
+    depositField.value = '';
+})
